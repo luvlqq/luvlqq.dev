0b70d3a4ba3f46a46d5754da50e47e2b0753214c
luvlqq <lualgod92@gmail.com>
2023-05-14 22:46:37 +0300

    analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,9 @@
-<script>
+<script lang="ts">
 	import '../app.css';
 	import Footer from '../components/footer.svelte';
+  import { dev } from '$app/environment';
+  import { inject } from '@vercel/analytics';
+  inject({ mode: dev ? 'development' : 'production' });
 </script>
 
 <slot />
