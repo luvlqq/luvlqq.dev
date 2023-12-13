ab0f8ca610e3b758f89141a85f4cc89051f1275b
luvlqq <lualgod92@gmail.com>
2023-12-13 19:20:08 +0300

    add speed insight

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,6 +3,7 @@
 	import Footer from '../components/footer.svelte';
 	import { dev } from '$app/environment';
 	import { inject } from '@vercel/analytics';
+	import { injectSpeedInsights } from "@vercel/speed-insights/sveltekit"
 	inject({ mode: dev ? 'development' : 'production' });
 </script>
 
