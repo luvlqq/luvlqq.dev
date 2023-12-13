38d92912916686d660260af65df00c9607e4e416
luvlqq <lualgod92@gmail.com>
2023-12-13 19:25:43 +0300

    fix import

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,7 +3,6 @@
 	import Footer from '../components/footer.svelte';
 	import { dev } from '$app/environment';
 	import { inject } from '@vercel/analytics';
-	import { injectSpeedInsights } from "@vercel/speed-insights/sveltekit"
 	inject({ mode: dev ? 'development' : 'production' });
 </script>
 
